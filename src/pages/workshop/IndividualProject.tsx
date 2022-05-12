b88f9488c9bcff1555be4e3fac762dd3c719611a
Philip Lewis <philipjonlewis@gmail.com>
2022-05-13 01:38:01 +0800

    need to refactor redux slices.  have one for project lists only and then another one for the current and or active project

--- src/pages/workshop/IndividualProject.tsx
+++ src/pages/workshop/IndividualProject.tsx
@@ -3,8 +3,25 @@ import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const IndividualProject = () => {
-  const { currentProject } = useSelector((state) => state.projects);
-  return <div>{currentProject.id}</div>;
+  const {
+    currentProject: { id, projectName, projectPhases },
+  } = useSelector((state) => state.projects);
+  return (
+    <div>
+      <p> {id} </p>
+      <p>{projectName}</p>
+      {projectPhases.map((phase) => {
+        return (
+          <div key={phase.phaseId}>
+            <hr />
+            <p>{phase.phaseId}</p>
+            <p>{phase.phaseCount}</p>
+            <p>{phase.phaseName}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default IndividualProject;
--- src/pages/workshop/Projects.tsx
+++ src/pages/workshop/Projects.tsx
@@ -2,16 +2,19 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Outlet, Link } from "react-router-dom";
 import { ProjectList } from "../../components";
+import { useParams } from "react-router-dom";
 
 const Projects = () => {
+  const { projectId } = useParams();
   const { currentProject } = useSelector((state) => state.projects);
   const [projectForm, setProjectForm] = useState(false);
   const [selectedProject, setSelectedProject] = useState(false);
+  // console.log(projectId);
 
   return (
     <>
       <div className="projects-page">
-        {!currentProject.status && <ProjectList />}
+        {!projectId && <ProjectList />}
 
         <Outlet />
       </div>
