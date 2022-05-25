96c9870b961b5442e5eb495973a0e4c8b2eca19a
Philip Lewis <philipjonlewis@gmail.com>
2022-05-25 23:20:06 +0800

    fixing some tasks and taskette styling

--- src/components/navigation/ProjectNavbar.tsx
+++ src/components/navigation/ProjectNavbar.tsx
@@ -77,9 +77,9 @@ const ProjectNavbar = () => {
             d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
           />
         </svg>
-        <p>Project Information</p>
+        <p>Project Details</p>
       </Link>
-
+      {/* 
       <div className="add-phase-container">
         <form action="#">
           <input
@@ -109,7 +109,7 @@ const ProjectNavbar = () => {
             </svg>
           </button>
         </form>
-      </div>
+      </div> */}
 
       <div className="phase-information-container">
         {/* <div className="phase-title-container">
