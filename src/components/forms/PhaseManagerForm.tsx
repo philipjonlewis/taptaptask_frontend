10bc9df4ce448aa3d1821c55016e076b581f9c22
Philip Lewis <philipjonlewis@gmail.com>
2022-05-28 20:59:41 +0800

    opted to not use framer motions reorder due to some curosr bugs.  Might use react dnd

--- src/components/forms/PhaseManagerForm.tsx
+++ src/components/forms/PhaseManagerForm.tsx
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
-import { Reorder, AnimatePresence, useDragControls } from "framer-motion";
 
 const PhaseManagerForm = () => {
-  const [items, setItems] = useState([0, 1, 2, 3]);
-  console.log(items);
-  const controls = useDragControls();
-
   return (
     <div className="phase-management-container">
       <div className="title-container">
@@ -13,25 +8,9 @@ const PhaseManagerForm = () => {
       </div>
 
       <div className="interactive-phase-container">
-        <Reorder.Group values={items} onReorder={setItems} axis="y">
-          {items.map((item) => (
-            <Reorder.Item
-              // initial={{ opacity: 0 }}
-              // animate={{ opacity: 1 }}
-              // exit={{ opacity: 0 }}
-              key={item}
-              value={item}
-              //   dragListener={false}
-              //   dragControls={controls}
-            >
-              <span> {item}</span>
-              {/* <div
-                className="reorder-handle"
-                onPointerDown={(e) => controls.start(e)}
-              /> */}
-            </Reorder.Item>
-          ))}
-        </Reorder.Group>
+        <ul>
+          <li></li>
+        </ul>
       </div>
     </div>
   );
